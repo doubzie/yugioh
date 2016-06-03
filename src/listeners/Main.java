4b205a2d77c780fa498c737f42bac288c42297b1
ar-maged <abdulrahman.maged95@gmail.com>
2016-06-03 22:05:20 +0200

    Format code in Main.java

--- src/listeners/Main.java
+++ src/listeners/Main.java
@@ -6,17 +6,13 @@ import gui.GUI;
 public class Main {
 
 	public Main() {
-
 		Board board = new Board();
 		GUI gui = new GUI();
 		new Controller(board, gui);
-
 	}
 
 	public static void main(String[] args) {
-
 		new Main();
-
 	}
 
 }
