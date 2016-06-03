a51eb8fc2b8b01b40606d6eefdf606be3b918237
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2016-06-03 23:13:17 +0200

    Fixing imports in first interface

--- src/gui/FirstInterface.java
+++ src/gui/FirstInterface.java
@@ -4,11 +4,10 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.MediaTracker;
-import java.awt.Toolkit;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
